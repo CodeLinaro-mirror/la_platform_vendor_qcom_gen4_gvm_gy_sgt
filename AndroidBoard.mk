be4a9ab8efb5a8d2a9182b373e7c72c6475abd43
Vijayakumar Badiger <vbadiger@qti.qualcomm.com>
2026-05-28 02:14:51 -0700

    gen4_gvm_gy_sgt: add radio folder for ota support
    Added radio folder for supporting the OTA package generation.
    
    Change-Id: I6177bcd5aac276d1ed50975fafa0bd276abd54aa
    Signed-off-by: Vijayakumar Badiger <vbadiger@qti.qualcomm.com>
    (cherry picked from commit e64ab77f735f17c406c60f00015e6cfd52d9fd2d)
    (cherry picked from commit e692720dc9de067f7c557acce820796e33617c11)

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,2 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+
+#----------------------------------------------------------------------
+# Radio image
+#----------------------------------------------------------------------
+ifeq ($(ADD_RADIO_FILES), true)
+radio_dir := $(LOCAL_PATH)/radio
+RADIO_FILES := $(shell cd $(radio_dir) ; ls)
+$(foreach f, $(RADIO_FILES), \
+        $(call add-radio-file,radio/$(f)))
+endif
 # Include the base products AndroidBoard.mk
 include device/qcom/gen4_gvm_gy/AndroidBoard.mk
